73c32486b309262729fee3ecaae5740b2861ea9c
Andreas Gudian <agudian@apache.org>
2016-09-19 20:35:19 +0200

    Add an example for tycho-compiler-jdt

---
+++ real-world-mapstruct-with-tycho/src/main/java/sample/Target.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Target
+{
+    private String item;
+
+    /**
+     * @return the item
+     */
+    public String getItem()
+    {
+        return item;
+    }
+
+    /**
+     * @param item the item to set
+     */
+    public void setItem( String item )
+    {
+        this.item = item;
+    }
+}
